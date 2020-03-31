edd3d682b37b40e3d965b876e9aa50ee37600785
Abdulrahman Mohamed Abdulrahman <abdulrahman.mohd1996@gmail.com>
2020-03-31 18:20:35 +0200

    creating the msp.h File

---
+++ course1/include/msp432/msp.h
@@ -0,0 +1,19 @@
+/**
+ * @msp.h
+ * @brief This file is to be used to include the MSP driver files
+ *
+ * @author Abdulrahman Mohamed
+ * @date March 31, 2020
+ *
+ */
+
+#ifndef __MSP__
+#define __MSP__
+
+
+#include "msp_compatibility.h"
+#include "msp432p401r.h"
+#include "system_msp432p401r.h"
+
+
+#endif
